d396d8f69f7ce4bda9a2fb6454f03e20ebbf937d
Choi Jae Young <chlwodud0327@naver.com>
2024-01-17 14:05:01 +0900

    Feat: 백준 문제 2743번

---
+++ src/solution/level5/Question2743.java
@@ -0,0 +1,14 @@
+package solution.level5;
+
+import java.io.*;
+
+public class Question2743 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String inputData = br.readLine();
+
+        bw.write(inputData.length()+"");
+        bw.flush();
+    }
+}
