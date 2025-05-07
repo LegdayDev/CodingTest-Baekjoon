56978a99ad5dc830d43507c5a58c6348ded02949
chlwodud0327 <chlwodud0327@naver.com>
2025-05-07 20:42:05 +0900

    250507 클래스1 마지막 푼제 푸는중

---
+++ src/solution/others/Question31403.java
@@ -0,0 +1,23 @@
+package solution.others;
+
+import java.io.*;
+
+public class Question31403 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String A = br.readLine();
+        String B = br.readLine();
+        String C = br.readLine();
+
+        int result1 = (Integer.parseInt(A)+Integer.parseInt(B))-Integer.parseInt(C);
+        int result2 = Integer.parseInt(A+B)-Integer.parseInt(C);
+
+        bw.write(result1+"\n");
+        bw.write(result2+"\n");
+
+        br.close();
+        bw.close();
+    }
+}
