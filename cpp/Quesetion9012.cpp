70cf2872dd5d98f56c2ee57b7ba33a2f132a9232
chlwodud0327 <chlwodud0327@naver.com>
2025-07-14 15:29:19 +0900

    🎨 9012 문제 해결(c++)

---
+++ cpp/Quesetion9012.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+int main()
+{
+  int testcase;
+  cin >> testcase;
+
+  while (testcase--)
+  {
+
+    // string line;
+    // cin >> line;
+
+    // stack<char> st;
+
+    // for (char c : line)
+    // {
+    //   if (!st.empty() && st.top() == '(' && c == ')')
+    //     st.pop(); // 짝 달라야 제거
+    //   else
+    //     st.push(c); // 아니면 넣기
+    // }
+
+    // if (st.empty())
+    //   cout << "YES" << endl;
+    // else
+    //   cout << "NO" << endl;
+  }
+
+  return 0;
+}
